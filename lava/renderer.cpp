9143dc96814a302e8532904e6eb57f97a3dbc211
Jamie Morgan <jamiemorgan.dev@gmail.com>
2020-05-14 03:44:07 +0100

    selecting physical device based on features and properties

--- lava/renderer.cpp
+++ lava/renderer.cpp
@@ -14,6 +14,7 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(VkDebugUtilsMessageSeverity
 
 lava_renderer::lava_renderer(lava_app * app)
 {
+    // Register application information
     VkApplicationInfo app_info = {};
     app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     app_info.pApplicationName = app->title;
@@ -37,6 +38,7 @@ lava_renderer::lava_renderer(lava_app * app)
     auto extensions = lvk::filter_supported_extensions(requested_extensions);
     auto layers = lvk::filter_supported_layers(requested_layers);
     
+    // Create Vulkan instance
     VkInstanceCreateInfo instance_create_info = {};
     instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     instance_create_info.pApplicationInfo = &app_info;
@@ -51,20 +53,48 @@ lava_renderer::lava_renderer(lava_app * app)
     debug_create_info.pfnUserCallback = debug_callback;
     instance_create_info.pNext = (VkDebugUtilsMessengerCreateInfoEXT *)&debug_create_info;
 #endif
-    if (vkCreateInstance(&instance_create_info, nullptr, &vk_instance) != VK_SUCCESS)
+    if (vkCreateInstance(&instance_create_info, nullptr, &vulkan_instance) != VK_SUCCESS)
     {
         throw std::runtime_error("Failed to create instance!");
     }
-
+    
 #if USE_VALIDATION
-    debug_messenger = lvk::make_debug_messenger(vk_instance, &debug_create_info);
+    debug_messenger = lvk::make_debug_messenger(vulkan_instance, &debug_create_info);
 #endif
+
+    // Find and select vulkan compatible device for use
+    uint32_t device_count;
+    vkEnumeratePhysicalDevices(vulkan_instance, &device_count, nullptr);
+    if (device_count == 0)
+        throw std::runtime_error("Couldn't find any GPUs with Vulkan support!");
+
+    std::vector<VkPhysicalDevice> physical_devices(device_count);
+    vkEnumeratePhysicalDevices(vulkan_instance, &device_count, physical_devices.data());
+
+    physical_device = VK_NULL_HANDLE;
+    // Select a GPU which has the following properties and features...
+    for (const auto & device : physical_devices)
+    {
+        VkPhysicalDeviceProperties properties;
+        VkPhysicalDeviceFeatures features;
+        vkGetPhysicalDeviceProperties(device, &properties);
+        vkGetPhysicalDeviceFeatures(device, &features);
+
+        if (properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU && features.geometryShader)
+        {
+            physical_device = device;
+            break;
+        }
+    }
+
+    if (physical_device == VK_NULL_HANDLE)
+        throw std::runtime_error("Couldn't find a GPU with appropriate features!");
 }
 
 lava_renderer::~lava_renderer()
 {
 #if USE_VALIDATION
-    lvk::destroy_debug_messenger(vk_instance, debug_messenger);
+    lvk::destroy_debug_messenger(vulkan_instance, debug_messenger);
 #endif
-    vkDestroyInstance(vk_instance, nullptr);
+    vkDestroyInstance(vulkan_instance, nullptr);
 }
--- lava/renderer.h
+++ lava/renderer.h
@@ -11,11 +11,9 @@ public:
     lava_renderer(lava_app * app);
     ~lava_renderer();
 
-    void setup_validation_layers(VkInstanceCreateInfo * create_info);
-    void setup_debug_messenger();
-
-    VkInstance vk_instance;
+    VkInstance vulkan_instance;
     VkDebugUtilsMessengerEXT debug_messenger;
+    VkPhysicalDevice physical_device;
 };
 
 #endif
