75130ef3c8b3a4a872a681ca02a01d65ab1958a3
Jamie Morgan <jamiemorgan.dev@gmail.com>
2020-07-12 12:43:50 +0100

    add tiny obj loader

--- lava/thirdparty_header_impl.cpp
+++ lava/thirdparty_header_impl.cpp
@@ -1,3 +1,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
 #undef STB_IMAGE_IMPLEMENTATION
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tinyobj/tinyobjloader.h>
+#undef TINYOBJLOADER_IMPLEMENTATION
